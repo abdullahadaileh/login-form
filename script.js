eb49cf2bba9242e6df37ff7f51f78f32875b748d
abdullahadaileh <abdullahadaileh957@gmail.com>
2025-04-21 18:41:54 +0300

    x

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); 
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  localStorage.setItem('user_name', name);
+  localStorage.setItem('user_email', email);
+  localStorage.setItem('user_password', password);
+
+  alert('Data saved to localStorage!');
+  form.reset();
+});
